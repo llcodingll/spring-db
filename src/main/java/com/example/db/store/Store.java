e2d2317f4cd93ac7bb771b3a9446c3db60caab98
유윤지 <ll.o.llwhkr@gmail.com>
2024-10-14 10:22:47 +0900

    nullable: false = notnull

--- src/main/java/com/example/db/store/Store.java
+++ src/main/java/com/example/db/store/Store.java
@@ -9,7 +9,7 @@ public class Store {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "store_id")
     private Long Id;
-    @Column(name = "store_name")
+    @Column(name = "store_name", nullable = false)
     private String storeName;
     @Column(name = "store_address")
     private String storeAddress;
